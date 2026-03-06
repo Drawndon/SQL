c3bc0c772f1a6cf7f7a72ca36ec2826425574e7e
Андрей Полищук <dcnfandrey@mail.ru>
2026-03-06 21:37:39 +0300

    Holidays DONE

---
+++ DML/SQLQuery7-SELECT DaysOFF.sql
@@ -0,0 +1,11 @@
+--SQLQuery7-SELECT DaysOFF.sql
+
+USE PV_521_Import;
+
+SELECT 
+		[Дата]		=	[date],
+		[Праздник]	=	holiday_name
+FROM	DaysOFF, Holidays
+WHERE holiday=holiday_id
+ORDER BY [date]
+;
